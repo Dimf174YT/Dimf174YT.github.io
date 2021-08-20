4cbf78959ac33f20a711499444d071073536d93e
Dimf174_YT <85029272+Dimf174YT@users.noreply.github.com>
2021-08-20 21:20:44 +0300

    Add files via upload

---
+++ lib/module/ui-main-menu.php
@@ -0,0 +1,141 @@
+<div class='menu-con-message fade-out-onload'>
+	<div class='menu-tx1-message toggle-message fade-out-onload'>
+		<span>We are currently experiencing a higher server volume than usual. We sincerely apologize for any inconveniences.</span>
+	</div>
+</div>
+<div class="window-con-theme">
+	<div class="window-bg-theme">
+	</div>
+	<div class="theme-con-spinner">
+		<div class="theme-ico-spinner">
+		</div>
+	</div>
+	<div class="theme-tx1-description pulsate">
+		<span>This feature is currently not yet available</span>
+	</div>
+	<div class="wavebar-con-container-theme">
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+</div>
+<div class="theme-btn-close toggle-theme">
+</div>
+<div class="settings-menu-con-outer settings-submenu">
+	<div class="settings-menu-con-wrapper toggle-darkmode settings-menu-upper">
+		<div class="settings-menu-ico-darkmode darkmode-invert">
+		</div>
+		<div class="settings-menu-btn-button btn-darkmode">
+			<span>Toggle Dark Mode</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-wrapper toggle-transparency">
+		<div class="settings-menu-ico-transparency darkmode-invert">
+		</div>
+		<div class="settings-menu-btn-button btn-transparency">
+			<span>Transparency - On</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-wrapper toggle-particles">
+		<div class="settings-menu-ico-particles darkmode-invert">
+		</div>
+		<div class="settings-menu-btn-button btn-particles">
+			<span>Particles - On</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-wrapper toggle-pulsate">
+		<div class="settings-menu-ico-pulsate darkmode-invert">
+		</div>
+		<div class="settings-menu-btn-button btn-pulsate">
+			<span>Pulsate - On</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-wrapper toggle-waves">
+		<div class="settings-menu-ico-waves darkmode-invert">
+		</div>
+		<div class="settings-menu-btn-button btn-waves">
+			<span>Waves - On</span>
+		</div>
+	</div>
+	<div class="settings-menu-con-wrapper toggle-theme settings-menu-lower" style="border-top: solid 1px rgba(0,0,0,.02);">
+		<div class="settings-menu-ico-theme darkmode-invert">
+		</div>
+		<div class="settings-menu-btn-button btn-theme">
+			<span>Change Theme</span>
+		</div>
+	</div>
+</div>
+<div class='menu-con-container'>
+	<div class="menu-con-backdrop">
+	</div>
+	<div class="menu-btn-settings toggle-settings">
+		<div class="menu-tx1-settings">
+			<span>Settings</span>
+		</div>
+	</div>
+	<div class="menu-con-outer">
+		<div class="menu-con-inner">
+			<div class="menu-con-logo">
+			</div>
+			<div class="mobile-menu-btn-open toggle-mobilemenu">
+			</div>
+			<a href='https:\\www.youtube.com\\channel\UCSdQri3XoPo4IW3Ei8AS9rg' target="_blank">
+			<div class="menu-btn-select">
+				<span>YouTube</span>
+			</div>
+			</a>
+			<a href="https://vk.com/id285878524" target="_blank">
+			<div class="menu-btn-select">
+				<span>VK OFFICIAL</span>
+			</div>
+			</a>
+			<a href='https://vk.com/id510994757' target="_blank">
+			<div class="menu-btn-select">
+				<span>VK Fake</span>
+			</div>
+			</a>
+			<a href='https://t.me/Dimf174_YT_bot' target="_blank">
+			<div class="menu-btn-select">
+				<span>Telegram BOT</span>
+			</div>
+		</div>
+	</div>
+</div>
+<div class='mobile-menu-con-container popup-mobilemenu' style="display:none;">
+	<div class="menu-con-backdrop-2">
+	</div>
+	<div class='mobile-menu-con-scroll'>
+		<div class="mobile-menu-con-outter">
+			<div class="mobile-menu-con-inner" style="overflow: hidden !important;">
+				<a href="/">
+				<div class='mobile-menu-btn-select'>
+					<span>Home</span>
+				</div>
+				</a>
+				<a href="https:\\www.youtube.com\\channel\UCSdQri3XoPo4IW3Ei8AS9rg">
+				<div class='mobile-menu-btn-select'>
+					<span>YouTube</span>
+				</div>
+				</a>
+				<a href="https://vk.com/id285878524">
+				<div class='mobile-menu-btn-select'>
+					<span>VK OFFICIAL</span>
+				</div>
+				</a>
+				<a href="https://vk.com/id510994757">
+				<div class='mobile-menu-btn-select'>
+					<span>VK Fake</span>
+				</div>
+				</a>
+				<a href="https://t.me/Dimf174_YT_bot">
+				<div class='mobile-menu-btn-select'>
+					<span>Telegram BOT</span>
+				</div>
+			</a>
+			</div>
+		</div>
+	</div>
+</div>
